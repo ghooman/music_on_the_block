5e9a2b288b0012f120054ed39f4e743922fdb028
pung8146 <pung8146@naver.com>
2025-02-05 18:05:08 +0900

    상훈 feat : 마이 페이지 작업중

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -6,6 +6,7 @@ import demoFlag from "../assets/images/mypage/demo-flag.png";
 import gearImg from "../assets/images/mypage/gear.svg";
 import demoSocial from "../assets/images/mypage/instar.png";
 import demoChart from "../assets/images/mypage/demo-chart.png";
+import demoChart2 from "../assets/images/mypage/demo-chart2.png";
 const MyPage = () => {
   const [selectedService, setSelectedService] = useState("AI Services");
   const [selectedAiService, setSelectedAiService] = useState(
@@ -124,6 +125,55 @@ const MyPage = () => {
       <div className="mypage__chart">
         <img src={demoChart} alt="chart" />
       </div>
+
+      <section className="mypage__ai-status">
+        <p className="ai-status__title">AI Service Status</p>
+        <div className="ai-status__menu">
+          <button className="ai-status__menu-item active">All</button>
+          <button className="ai-status__menu-item">Lyric</button>
+          <button className="ai-status__menu-item">Songwritiy</button>
+          <button className="ai-status__menu-item">Lyric & Songwritiy</button>
+        </div>
+
+        <div className="ai-status__info">
+          <div className="ai-status__chart">
+            <img src={demoChart2} alt="chart" />
+          </div>
+          <div className="ai-status__detail">
+            <p className="ai-status__detail-title">AI Service Detail</p>
+            <div className="ai-status__detail-box">
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Top Tags</p>
+                <p className="detail-item__value">Love</p>
+              </div>
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Total Creation</p>
+                <p className="detail-item__value">239</p>
+              </div>
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Total Cost</p>
+                <p className="detail-item__value">5,304 MOB</p>
+              </div>
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Top Game</p>
+                <p className="detail-item__value">Ballad</p>
+              </div>
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Top Style</p>
+                <p className="detail-item__value">Romantic</p>
+              </div>
+              <div className="ai-status__detail-item">
+                <p className="detail-item__title">Last Used Date</p>
+                <p className="detail-item__value">
+                  Sat, 04 Nov 2023 14:40:00 UTC+0
+                  <br />
+                  Sat, 04 Nov 2023 14:40:00 UTC+9
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

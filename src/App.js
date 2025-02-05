c183db55fa241a1b58342b5ae25c69dec9de951c
15rudtp <thdrudtp15@naver.com>
2025-02-05 17:34:01 +0900

    크리에이트 첫 페이지

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Intro from "./components/Intro";
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Intro from './components/Intro';
 import {
-  BrowserRouter,
-  Link,
-  Route,
-  Router,
-  Routes,
-  useLocation,
-  // useNavigate,
-} from "react-router-dom";
-import Album from "./pages/Album";
-
+    BrowserRouter,
+    Link,
+    Route,
+    Router,
+    Routes,
+    useLocation,
+    // useNavigate,
+} from 'react-router-dom';
+import Album from './pages/Album';
+import Create from './pages/Create';
 
 function Layout({ children }) {
-  return (
-    <div>
-      <Header /> {/* 인트로 페이지를 제외한 모든 페이지에 헤더가 포함됨 */}
-      {children}
-    </div>
-  );
+    return (
+        <div>
+            <Header /> {/* 인트로 페이지를 제외한 모든 페이지에 헤더가 포함됨 */}
+            {children}
+        </div>
+    );
 }
 
-
-
 function App() {
-
-  return (
-    <div className="App">
-      <title>MUSIC ON THE BLOCK</title>
-      <Routes>
-        <Route path="/" element={<Intro />} /> {/* 인트로에는 헤더 X */}
-        <Route
-          path="/Album"
-          element={<Layout/>}
-        />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <title>MUSIC ON THE BLOCK</title>
+            <Routes>
+                <Route path="/" element={<Intro />} /> {/* 인트로에는 헤더 X */}
+                <Route path="/Album" element={<Layout />} />
+                <Route path="/create" element={<Create />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,25 +1,19 @@
-import "../styles/Album.scss";
-import React, { useEffect, useRef, useState } from "react";
+import '../styles/Album.scss';
+import React, { useEffect, useRef, useState } from 'react';
 
 // 이미지
 // import pass_img from "../assets/images/pass-img.png";
 
-
-import { Link } from "react-router-dom";
-
-
+import { Link } from 'react-router-dom';
 
 function Album({ setClickMenu }) {
+    const [activeTab, setActiveTab] = useState('Activation');
 
-  const [activeTab, setActiveTab] = useState('Activation');
-
-  return (
-    <>
-      <div className="album">
-        asdgasdgasdfghsdfhg
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="album">asdgasdgasdfghsdfhg</div>
+        </>
+    );
 }
 
 export default Album;

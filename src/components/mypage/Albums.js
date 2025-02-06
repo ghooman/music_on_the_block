46e3bd2e1f08d01663ea685da5dde5a32499153b
pung8146 <pung8146@naver.com>
2025-02-06 15:46:17 +0900

    상훈 feat : 마이앨범 수정중

--- src/components/mypage/Albums.js
+++ src/components/mypage/Albums.js
@@ -12,95 +12,110 @@ import Filter from "../filter/Filter";
 const Albums = () => {
   return (
     <div className="albums">
-      <h1 className="albums__title">Top Albums</h1>
-      <Filter />
-
-      <div className="albums__list">
-        <div className="albums__item">
-          <p className="albums__item-title">Top Likes</p>
-          <div className="albums__item-intro">
-            <div className="albums__item__img-box">
-              <img className="albums__item__img" src={demoAlbum} alt="album" />
-            </div>
-            <div className="albums__item-desc">
-              <p className="albums__item-desc-text">
-                he dances through his masks like breathing - Yolkhead
-              </p>
-              <div className="albums__item__play-count">
-                <img src={heartIcon} alt="play" />
-                <span>145</span>
-              </div>
-              <div className="albums__item__user-info">
+      <section className="albums__list">
+        <div className="albums__header">
+          <h1 className="albums__title">Top Albums</h1>
+          <Filter />
+        </div>
+        <div className="albums__body">
+          <div className="albums__item">
+            <p className="albums__item-title">Top Likes</p>
+            <div className="albums__item-intro">
+              <div className="albums__item__img-box">
                 <img
-                  className="albums__item__user-img"
-                  src={demoUser}
-                  alt="user"
+                  className="albums__item__img"
+                  src={demoAlbum}
+                  alt="album"
                 />
-                <span>Yolkhead</span>
-                <button className="albums__item__more-btn">
-                  <img src={moreIcon} alt="more" />
-                </button>
+              </div>
+              <div className="albums__item-desc">
+                <p className="albums__item-desc-text">
+                  he dances through his masks like breathing - Yolkhead
+                </p>
+                <div className="albums__item__play-count">
+                  <img src={heartIcon} alt="play" />
+                  <span>145</span>
+                </div>
+                <div className="albums__item__user-info">
+                  <img
+                    className="albums__item__user-img"
+                    src={demoUser}
+                    alt="user"
+                  />
+                  <span>Yolkhead</span>
+                  <button className="albums__item__more-btn">
+                    <img src={moreIcon} alt="more" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="albums__item">
+            <p className="albums__item-title">Top Comments</p>
+            <div className="albums__item-intro">
+              <div className="albums__item__img-box">
+                <img
+                  className="albums__item__img"
+                  src={demoAlbum}
+                  alt="album"
+                />
+              </div>
+              <div className="albums__item-desc">
+                <p className="albums__item-desc-text">
+                  he dances through his masks like breathing - Yolkhead
+                </p>
+                <div className="albums__item__play-count">
+                  <img src={commentIcon} alt="play" />
+                  <span>326</span>
+                </div>
+                <div className="albums__item__user-info">
+                  <img
+                    className="albums__item__user-img"
+                    src={demoUser}
+                    alt="user"
+                  />
+                  <span>Yolkhead</span>
+                  <button className="albums__item__more-btn">
+                    <img src={moreIcon} alt="more" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="albums__item">
+            <p className="albums__item-title">Top Plays</p>
+            <div className="albums__item-intro">
+              <div className="albums__item__img-box">
+                <img
+                  className="albums__item__img"
+                  src={demoAlbum}
+                  alt="album"
+                />
+              </div>
+              <div className="albums__item-desc">
+                <p className="albums__item-desc-text">
+                  he dances through his masks like breathing - Yolkhead
+                </p>
+                <div className="albums__item__play-count">
+                  <img src={playIcon} alt="play" />
+                  <span>326K</span>
+                </div>
+                <div className="albums__item__user-info">
+                  <img
+                    className="albums__item__user-img"
+                    src={demoUser}
+                    alt="user"
+                  />
+                  <span>Yolkhead</span>
+                  <button className="albums__item__more-btn">
+                    <img src={moreIcon} alt="more" />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="albums__item">
-          <p className="albums__item-title">Top Comments</p>
-          <div className="albums__item-intro">
-            <div className="albums__item__img-box">
-              <img className="albums__item__img" src={demoAlbum} alt="album" />
-            </div>
-            <div className="albums__item-desc">
-              <p className="albums__item-desc-text">
-                he dances through his masks like breathing - Yolkhead
-              </p>
-              <div className="albums__item__play-count">
-                <img src={commentIcon} alt="play" />
-                <span>326</span>
-              </div>
-              <div className="albums__item__user-info">
-                <img
-                  className="albums__item__user-img"
-                  src={demoUser}
-                  alt="user"
-                />
-                <span>Yolkhead</span>
-                <button className="albums__item__more-btn">
-                  <img src={moreIcon} alt="more" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="albums__item">
-          <p className="albums__item-title">Top Plays</p>
-          <div className="albums__item-intro">
-            <div className="albums__item__img-box">
-              <img className="albums__item__img" src={demoAlbum} alt="album" />
-            </div>
-            <div className="albums__item-desc">
-              <p className="albums__item-desc-text">
-                he dances through his masks like breathing - Yolkhead
-              </p>
-              <div className="albums__item__play-count">
-                <img src={playIcon} alt="play" />
-                <span>326K</span>
-              </div>
-              <div className="albums__item__user-info">
-                <img
-                  className="albums__item__user-img"
-                  src={demoUser}
-                  alt="user"
-                />
-                <span>Yolkhead</span>
-                <button className="albums__item__more-btn">
-                  <img src={moreIcon} alt="more" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </section>
 
       <section className="albums__my-albums">
         <div className="my-albums__header">

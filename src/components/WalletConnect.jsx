63083960af403437c10d510a380f8cbea2ae8549
dudwlswmd <sksmsrhtn113@naver.com>
2025-03-25 15:37:14 +0900

    영진 수정 - 크리에이트 & 언어 & 앨범디테일 수정중

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { createThirdwebClient } from "thirdweb";
-import { ConnectButton } from "thirdweb/react";
+// import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, useActiveWallet } from "thirdweb/react";
 import { createWallet, inAppWallet } from "thirdweb/wallets";
 
-export const WalletConnect = () => {
+export const WalletConnect = ({setLogin}) => {
   const client = createThirdwebClient({
     clientId: process.env.REACT_APP_THIRDWEB_CLIENT_ID,
   });
@@ -19,6 +21,13 @@ export const WalletConnect = () => {
     createWallet("io.rabby"),
     createWallet("io.zerion.wallet"),
   ];
+
+  const activeWallet = useActiveWallet();
+
+  // 연결 상태 감지해서 로그인 상태 전달
+  useEffect(() => {
+    setLogin(!!activeWallet);
+  }, [activeWallet, setLogin]);
 
   return <ConnectButton client={client} wallets={wallets} />;
 };

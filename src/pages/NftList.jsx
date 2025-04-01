a90b8fbe2301efad59cb343bb1f73edefd8f06bc
15rudtp <thdrudtp15@naver.com>
2025-04-01 12:35:57 +0900

    푸시

--- src/pages/NftList.jsx
+++ src/pages/NftList.jsx
@@ -5,6 +5,7 @@ import { CollectionItemList, NftItemList } from '../components/nft/NftItem';
 import Pagination from '../components/nft/Pagination';
 import Search from '../components/nft/Search';
 import FilterItems from '../components/nft/FilterItems';
+import { InfoRowWrap } from '../components/nft/InfoRow';
 
 const NftList = () => {
     const [selectCategory, setSelectCategory] = useState('NFT item');
@@ -17,6 +18,12 @@ const NftList = () => {
             {selectCategory === 'NFT item' && <NftItemList data={[1, 2, 3, 4, 5, 6, 7, 7, 8]} />}
             {selectCategory === 'Collection' && <CollectionItemList data={[1, 2, 3, 4, 5, 6, 7, 7, 8]} />}
             <Pagination />
+            <InfoRowWrap row={4}>
+                <InfoRowWrap.ValueItem title="Total Volume" value="하하하3하" />
+                <InfoRowWrap.ValueItem title="Total Volume" value="히히히히" />
+                <InfoRowWrap.ValueItem title="Total Volume" value="히하ㅓdf히ㅏ허" />
+                <InfoRowWrap.ValueItem title="Total Volume" value="히하ㅓ히ㅏ허" />
+            </InfoRowWrap>
         </ContentWrap>
     );
 };

d26dbdc6d0f0aa4d426a946950ffe457f59cd7eb
dudwlswmd <sksmsrhtn113@naver.com>
2025-04-01 18:43:31 +0900

    영진 수정 컴포넌트 분리

--- src/pages/Nft.jsx
+++ src/pages/Nft.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ContentWrap from '../components/nft/ContentWrap';
+import { NftItemList, CollectionItemList } from '../components/nft/NftItem';
+import { NftSlider } from '../components/nft/NftSlider';
+import { NftGraph } from '../components/nft/NftGraph';
 import Search from '../components/nft/Search';
-
-import { NftItemList, CollectionItemList,NftSwiper,GraphItem } from '../components/nft/NftItem';
+import nftBanner from '../assets/images/nft/nft-banner.png';
 import { InfoRowWrap } from '../components/nft/InfoRow';
-
 import '../styles/Nft.scss';
 
 const Nft = () => {
@@ -19,7 +20,7 @@ const Nft = () => {
                 <CollectionItemList data={[1, 2, 3]} />
             </ContentWrap>
             <ContentWrap title="Popular Genre">
-                <NftSwiper />
+                <NftSlider />
             </ContentWrap>
             <ContentWrap title="Data">
                 <InfoRowWrap row={4}>
@@ -28,7 +29,7 @@ const Nft = () => {
                     <InfoRowWrap.ValueItem title="Total Volume" value="3,224" />
                     <InfoRowWrap.ValueItem title="Total Volume" value="4,359" />
                 </InfoRowWrap>
-                <GraphItem />
+                <NftGraph />
             </ContentWrap>
         </div>
     );

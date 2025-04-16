1d323cbaef0c8b573c91f5301c64b78db4c74507
15rudtp <thdrudtp15@naver.com>
2025-04-16 13:35:10 +0900

    경세 앨범 정렬 필터 추가 및 앨범 페이지

--- src/components/mypage/Albums.jsx
+++ src/components/mypage/Albums.jsx
@@ -10,9 +10,11 @@ import subBannerImage4 from '../../assets/images/create/subbanner-bg4.png';
 import './Albums.scss';
 
 const Albums = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
 
     const albumSort = searchParams.get('album_sort');
+    const page = searchParams.get('page');
+    const search = searchParams.get('search');
 
     return (
         <div className="albums">
@@ -24,7 +26,7 @@ const Albums = () => {
             </SubBanner>
             <ContentWrap title="Albums List">
                 <ContentWrap.SubWrap gap={8}>
-                    <Filter />
+                    <Filter albumSort={true} />
                     <Search placeholder="Search by album name..." reset={{ page: 1 }} />
                 </ContentWrap.SubWrap>
                 {/** */}

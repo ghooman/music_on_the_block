083eb4df0f1b1d170234e1f0b798352739df0e00
15rudtp <thdrudtp15@naver.com>
2025-04-17 16:54:39 +0900

    경세 앨범 수정

--- src/components/unit/NoneContent.jsx
+++ src/components/unit/NoneContent.jsx
@@ -1,9 +1,10 @@
 import offImage from '../../assets/images/none-icon.svg';
+import NoDataImage from '../../assets/images/mypage/albums-no-data.svg';
 import loadingImg from '../../assets/images/loading-img.gif';
 
 import './NoneContent.scss';
 
-const NoneContent = ({ height, image = offImage, message, style, loading = false }) => {
+const NoneContent = ({ height, image = NoDataImage, message, style, loading = false }) => {
     if (loading) {
         image = loadingImg;
         message = 'Loading';

8645fdbc2bce9b27084b8a994cb87b62fa0533a3
15rudtp <thdrudtp15@naver.com>
2025-04-18 17:01:42 +0900

    경세 푸시

--- src/components/mypage/albums/EditAlbumSongs.jsx
+++ src/components/mypage/albums/EditAlbumSongs.jsx
@@ -1,29 +1,31 @@
-import React, { useState, useRef } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import ContentWrap from '../unit/ContentWrap';
-import Filter from '../unit/Filter';
-import Pagination from '../unit/Pagination';
-import Search from '../unit/Search';
-import SubCategories from '../unit/SubCategories';
-import SongPlayTable from '../unit/SongPlayTable'; // SongPlayTable 추가
+import React, { useState, useRef, useContext, useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+import ContentWrap from '../../unit/ContentWrap';
+import Filter from '../../unit/Filter';
+import Pagination from '../../unit/Pagination';
+import Search from '../../unit/Search';
+import SubCategories from '../../unit/SubCategories';
+import SongPlayTable from '../../unit/SongPlayTable'; // SongPlayTable 추가
 import axios from 'axios'; // axios 임포트
 
 // 이미지/아이콘들
-import songImg from '../../assets/images/intro/intro-demo-img2.png';
-import addIcon from '../../assets/images/add-icon.svg';
-import dellIcon from '../../assets/images/delete-icon.svg';
-import editIcon from '../../assets/images/edit.svg';
+import songImg from '../../../assets/images/intro/intro-demo-img2.png';
+import addIcon from '../../../assets/images/add-icon.svg';
+import dellIcon from '../../../assets/images/delete-icon.svg';
+import editIcon from '../../../assets/images/edit.svg';
 
 import './EditAlbumSongs.scss';
-import track2 from '../../assets/music/MusicOnTheBlock_v1.mp3';
-import track3 from '../../assets/music/nisoft_song.mp3';
+import track2 from '../../../assets/music/MusicOnTheBlock_v1.mp3';
+import track3 from '../../../assets/music/nisoft_song.mp3';
 
 // AI 관련 아이콘들
-import LyricsIcon from '../../assets/images/icon/Lyrics-Icon.svg';
-import LyricsAndSongwritingIcon from '../../assets/images/icon/Songwriting-Icon.svg';
-import SongwritingIcon from '../../assets/images/icon/Composition-Icon.svg';
-import SongPlayEditTable from '../unit/SongPlayEditTable';
-import EditAlbumModal from '../EditAlbumModal';
+import LyricsIcon from '../../../assets/images/icon/Lyrics-Icon.svg';
+import LyricsAndSongwritingIcon from '../../../assets/images/icon/Songwriting-Icon.svg';
+import SongwritingIcon from '../../../assets/images/icon/Composition-Icon.svg';
+import SongPlayEditTable from '../../unit/SongPlayEditTable';
+import EditAlbumModal from '../../EditAlbumModal';
+import { useQuery } from 'react-query';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 // 더미 데이터
 const dummySongsList = {
@@ -59,17 +61,41 @@ const dummySongsList = {
     total_cnt: 3,
 };
 
-const EditAlbumSongs = ({ token }) => {
-    const subCategoryList = [
-        { name: 'AI Lyrics & Songwriting', image: LyricsAndSongwritingIcon, preparing: false },
-        { name: 'AI Singing Evaluation', image: LyricsIcon, preparing: true },
-        { name: 'AI Cover Creation', image: SongwritingIcon, preparing: true },
-    ];
-    const [selected, setSelected] = useState(subCategoryList[0].name);
+const subCategoryList = [
+    { name: 'AI Lyrics & Songwriting', image: LyricsAndSongwritingIcon, preparing: false },
+    { name: 'AI Singing Evaluation', image: LyricsIcon, preparing: true },
+    { name: 'AI Cover Creation', image: SongwritingIcon, preparing: true },
+];
 
+const serverApi = process.env.REACT_APP_SERVER_API;
+
+const EditAlbumSongs = () => {
+    const [selected, setSelected] = useState(subCategoryList[0].name);
     const [iseditAlbumModal, setIsEditAlbumModal] = useState(false);
     const [activeSong, setActiveSong] = useState(null);
+    const [albumsBundleData, setAlbumsBundleData] = useState(null); // 앨범 원본 데이터
+    const [checkedSeletedSongs, setCheckedSelectedSongs] = useState([]);
+    const [checkedAlbumSongs, setCheckedAlbumSongs] = useState([]);
+
+    const { token } = useContext(AuthContext);
+    const { id } = useParams();
     const audioRef = useRef(null);
+
+    useEffect(() => {
+        const getAlbumsBundleData = async () => {
+            try {
+                const res = await axios.get(`${serverApi}/api/music/my/album/bundle/${id}/detail`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setAlbumsBundleData(res.data.song_list);
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        getAlbumsBundleData();
+    }, [id]);
 
     return (
         <>
@@ -86,14 +112,18 @@ const EditAlbumSongs = ({ token }) => {
                         />
                     </ContentWrap.SubWrap>
                     <SongPlayEditTable
-                        songList={dummySongsList.data_list}
+                        title="Selected Songs"
+                        checkHandler={setCheckedSelectedSongs}
+                        songList={albumsBundleData}
                         activeSong={activeSong}
                         setActiveSong={setActiveSong}
                         audioRef={audioRef}
                     />
                     <AddDeleteBtn />
                     <SongPlayEditTable
-                        songList={dummySongsList.data_list}
+                        title="Album Songs"
+                        checkHandler={setCheckedAlbumSongs}
+                        songList={albumsBundleData}
                         activeSong={activeSong}
                         setActiveSong={setActiveSong}
                         audioRef={audioRef}

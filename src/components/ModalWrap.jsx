8cc5077bb6a4e72f508dfc3136add602efe36778
pung8146 <pung8146@naver.com>
2025-04-28 15:56:54 +0900

    상훈 feat : 새로운 NFT 컬렉션 생성 API 추가 및 기존 API 오류 메시지 개선. CreateCollectionModal에서 파일 크기 및 형식 검증 로직 추가. MintNftDetail에서 나의 NFT 컬렉션 조회 기능 구현. UI 개선 및 코드 정리.

--- src/components/ModalWrap.jsx
+++ src/components/ModalWrap.jsx
@@ -5,33 +5,33 @@ import modalCloseImg from '../assets/images/close.svg';
 import './ModalWrap.scss';
 
 const ModalWrap = ({ className = '', children, onClose, title = 'MODAL' }) => {
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = '';
-        };
-    }, []);
-
-    const modalClose = (e) => {
-        // if (e.target.className === 'modal-wrap') {
-        //     onClose(false);
-        // }
-        if (e.target.className.includes('modal-wrap')) {
-            onClose(false);
-        }
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
     };
+  }, []);
 
-    return (
-        <div className={`modal-wrap ${className}`} onClick={(e) => modalClose(e)}>
-            <div className="modal-content-box">
-                <div className="modal-header">
-                    <h2>{title}</h2>
-                    <img className="modal-close" src={modalCloseImg} onClick={() => onClose(false)} />
-                </div>
-                <div className="modal-content">{children}</div>
-            </div>
+  const modalClose = e => {
+    // if (e.target.className === 'modal-wrap') {
+    //     onClose(false);
+    // }
+    if (e.target.className.includes('modal-wrap')) {
+      onClose(false);
+    }
+  };
+
+  return (
+    <div className={`modal-wrap ${className}`} onClick={e => modalClose(e)}>
+      <div className="modal-content-box">
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <img className="modal-close" src={modalCloseImg} onClick={() => onClose(false)} />
         </div>
-    );
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ModalWrap;

8a3c6fd1a8bd10a3753caf4be5f2658c6160f92f
pung8146 <pung8146@naver.com>
2025-04-29 12:54:13 +0900

    상훈 feat : 백업 nft 돌려놓기진행중

--- src/pages/Nft.jsx
+++ src/pages/Nft.jsx
@@ -6,87 +6,45 @@ import { NftGraph } from '../components/nft/NftGraph';
 import Search from '../components/unit/Search';
 import { InfoRowWrap } from '../components/nft/InfoRow';
 import '../styles/Nft.scss';
-import { Link, useNavigate } from 'react-router-dom';
-import { getNftsMain, getNftsStatistics } from '../api/nfts/nftsMainApi';
-import PreparingModal from '../components/PreparingModal';
-
+import { Link } from 'react-router-dom';
+import { getNftsMain } from '../api/nfts/nftsMainApi';
 const Nft = () => {
   const [nftList, setNftList] = useState([]);
   const [collectionList, setCollectionList] = useState([]);
-  const [showPreparingModal, setShowPreparingModal] = useState(false);
-  const [nftStatistics, setNftStatistics] = useState({});
-  const navigate = useNavigate();
-
   useEffect(() => {
-    //=================
-    // 스테이티스틱스
-    //=================
-    const fetchNftStatistics = async () => {
-      try {
-        const response = await getNftsStatistics();
-        console.log(response?.data, '스테이티스틱스');
-        setNftStatistics(response.data);
-      } catch (e) {
-        console.error('Failed to fetch NFTs Statistics', e);
-      }
-    };
-
-    //=================
-    // 메인 데이터
-    //=================
     const fetchNftsMain = async () => {
       try {
         const response = await getNftsMain();
-        console.log(response.data, '리스폰스 데이터');
-        setNftList(response.data.nft_list);
-        setCollectionList(response.data.nft_collection_list);
+        setNftList(response.nft_list);
+        setCollectionList(response.collection_list);
       } catch (error) {
         console.error('Failed to fetch NFTs:', error);
       }
     };
-    fetchNftStatistics();
+
     fetchNftsMain();
   }, []);
-
   return (
     <div className="nft">
       <NftExchange />
-      <Search
-        placeholder="Search"
-        handler={search => navigate(`/nft/list?category?=NFT+item&page=1&search=${search}`)}
-      />
-      <ContentWrap title="TOP NFTs" link="/nft/list?category=NFT+item&page=1">
-        <NftItemList data={nftList} />
+      <Search placeholder="Search" />
+      <ContentWrap title="TOP NFTs" link="/nft/list">
+        <NftItemList data={[1, 2, 3, 4]} />
       </ContentWrap>
-      <ContentWrap title="Popular Collection" link="/nft/list?category=Collection&page=1">
-        <CollectionItemList data={collectionList} />
+      <ContentWrap title="Popular Collection" link="/nft/list">
+        <CollectionItemList data={[1, 2, 3]} />
       </ContentWrap>
       <ContentWrap title="Popular Genre">
         <NftSlider />
       </ContentWrap>
       <ContentWrap title="Data">
         <InfoRowWrap row={4}>
-          <InfoRowWrap.ValueItem
-            title="Total Volume"
-            value={nftStatistics?.total_price?.toLocaleString()}
-          />
-          <InfoRowWrap.ValueItem
-            title="Average Price"
-            value={nftStatistics?.avg_price?.toLocaleString()}
-          />
-          <InfoRowWrap.ValueItem
-            title="Number of NFTs Issued"
-            value={nftStatistics?.create_nft_cnt?.toLocaleString()}
-          />
-          <InfoRowWrap.ValueItem
-            title="Highest Deal Today"
-            value={nftStatistics?.today_max_price?.toLocaleString()}
-          />
+          <InfoRowWrap.ValueItem title="Total Volume" value="16,145" />
+          <InfoRowWrap.ValueItem title="Average Price" value="240" />
+          <InfoRowWrap.ValueItem title="Number of NFTs Issued" value="3,224" />
+          <InfoRowWrap.ValueItem title="Highest Deal Today" value="4,359" />
         </InfoRowWrap>
-        <NftGraph
-          barGraphData={nftStatistics?.total_transaction_price_progress}
-          lineGraphData={nftStatistics?.create_nft_progress}
-        />
+        <NftGraph />
       </ContentWrap>
     </div>
   );

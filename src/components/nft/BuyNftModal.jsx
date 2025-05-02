fdd9a51bbe2eadc62016888dd9c81b1a84643424
15rudtp <thdrudtp15@naver.com>
2025-05-02 10:01:47 +0900

    경세 push

--- src/components/nft/BuyNftModal.jsx
+++ src/components/nft/BuyNftModal.jsx
@@ -3,11 +3,19 @@ import ModalWrap from '../ModalWrap';
 
 import './BuyNftModal.scss';
 
-const BuyNftModal = () => {
+const BuyNftModal = ({ setBuyNftModal }) => {
   const [agree, setAgree] = useState(false);
 
+  const onClose = () => {
+    setBuyNftModal(false);
+  };
+
+  const handleBuy = () => {
+    if (!agree) return;
+  };
+
   return (
-    <ModalWrap title="Confirm buy NFT">
+    <ModalWrap title="Confirm buy NFT" onClose={onClose}>
       <div className="buy-nft-modal">
         <p className="buy-nft-modal__text">Buying "아이템 네임" transfers the NFT to your wallet</p>
         <ul className="buy-nft-modal__list">
@@ -15,15 +23,28 @@ const BuyNftModal = () => {
           <li className="buy-nft-modal__list--item">Price : 가격</li>
           <li className="buy-nft-modal__list--item">Network gas fees may apply</li>
         </ul>
-        <div className="buy-nft-modal__checkbox-wrap">
-          <input className="buy-nft-modal__checkbox" type="checkbox" />
+        <label className="buy-nft-modal__checkbox-wrap">
+          <input
+            className="buy-nft-modal__checkbox"
+            type="checkbox"
+            checked={agree}
+            onChange={() => setAgree(prev => !prev)}
+          />
           <p className="buy-nft-modal__checkbox--message">
             No refunds or cancellations after purchase
           </p>
-        </div>
+        </label>
         <div className="buy-nft-modal__button-wrap">
-          <button className="buy-nft-modal__button">Cancel</button>
-          <button className="buy-nft-modal__button">Buy NFT</button>
+          <button className="buy-nft-modal__button cancel-button" onClick={onClose}>
+            Cancel
+          </button>
+          <button
+            className="buy-nft-modal__button buy-button"
+            onClick={handleBuy}
+            disabled={!agree}
+          >
+            Buy NFT
+          </button>
         </div>
       </div>
     </ModalWrap>

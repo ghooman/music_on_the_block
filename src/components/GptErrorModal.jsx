0f5c0a19f12c56fb976edc8ca409d84f83d43266
pung8146 <pung8146@naver.com>
2025-06-23 13:02:06 +0900

    상훈 feat : GptErrorModal 컴포넌트의 props 수정 및 MelodyChatBot에서 에러 모달 사용 추가

--- src/components/GptErrorModal.jsx
+++ src/components/GptErrorModal.jsx
@@ -4,19 +4,13 @@ import ModalWrap from './ModalWrap';
 import './GptErrorModal.scss';
 import { useTranslation } from 'react-i18next';
 
-const GptErrorModal = ({ setPreparingModal }) => {
+const GptErrorModal = ({ setShowErrorModal, errorMessage }) => {
   const { t } = useTranslation('modal');
 
   return (
-    <ModalWrap
-      title={t('ERROR')}
-      onClose={() => setPreparingModal(false)}
-      className="error-modal"
-    >
-      <p className='error-modal__txt'>
-        {t('Please regenerate it again in a little while.')}
-      </p>
-      <button className="error-modal-btn" onClick={() => setPreparingModal(false)}>
+    <ModalWrap title={t('ERROR')} onClose={() => setShowErrorModal(false)} className="error-modal">
+      <p className="error-modal__txt">{t('Please regenerate it again in a little while.')}</p>
+      <button className="error-modal-btn" onClick={() => setShowErrorModal(false)}>
         {t('OK')}
       </button>
     </ModalWrap>
--- src/components/create/chatbot/MelodyChatBot.js
+++ src/components/create/chatbot/MelodyChatBot.js
@@ -4,6 +4,7 @@ import React, { useState, useContext, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import OpenAI from 'openai';
 import CreateLoading from '../../CreateLoading';
+import GptErrorModal from '../../GptErrorModal';
 import axios from 'axios';
 import { AuthContext } from '../../../contexts/AuthContext';
 import { useUserDetail } from '../../../hooks/useUserDetail';
@@ -46,6 +47,7 @@ const MelodyChatBot = ({
   const { token } = useContext(AuthContext);
   const { data: userData } = useUserDetail();
   const navigate = useNavigate();
+  const [showErrorModal, setShowErrorModal] = useState(false);
   const [showLyricsModal, setShowLyricsModal] = useState(false);
   // 선택된 언어에 따라 리소스 파일 선택
   const localeSongList = {
@@ -687,7 +689,8 @@ const MelodyChatBot = ({
       const formData = {
         album: {
           title: melody_title,
-          detail: Array.isArray(melody_detail) ? melody_detail.join(', ') : melody_detail || '',
+          // detail: Array.isArray(melody_detail) ? melody_detail.join(', ') : melody_detail || '',
+          detail: [],
           language: selectedLanguage,
           genre: standardizedGenre,
           style: '',
@@ -754,9 +757,11 @@ const MelodyChatBot = ({
         await musicGenerate(cover, generatedPrompt);
       } else {
         console.error('앨범 커버 생성에 실패하였습니다.');
+        setShowErrorModal(true);
       }
     } catch (error) {
       console.error('Error during song generation process:', error);
+      setShowErrorModal(true);
     } finally {
       setCreateLoading(false);
     }
@@ -906,6 +911,7 @@ const MelodyChatBot = ({
       {showLyricsModal && (
         <LyricsModal setShowLyricsModal={setShowLyricsModal} generatedLyric={generatedLyric} />
       )}
+      {showErrorModal && <GptErrorModal setShowErrorModal={setShowErrorModal} />}
     </div>
   );
 };
